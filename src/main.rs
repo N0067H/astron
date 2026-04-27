475f0d52e7a2c39e4439cdba09d81165f42e4415
SeungYeop Ryu <n006h3417h@gmail.com>
2026-04-27 20:05:26 +0900

    feat: integrate parser into main application flow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod ast;
 mod lexer;
+mod parser;
 pub mod token;
 
 use std::fs;
@@ -8,16 +9,6 @@ use token::TokenKind;
 fn main() {
     let source = fs::read_to_string("exam/mission.astrn").expect("failed to read mission.astrn");
     let tokens = lexer::tokenize(&source);
-
-    for tok in &tokens {
-        let label = match &tok.kind {
-            TokenKind::Ident(s) => format!("Ident({s})"),
-            TokenKind::StrLit(s) => format!("StrLit({s:?})"),
-            TokenKind::IntLit(n) => format!("IntLit({n})"),
-            TokenKind::FloatLit(f) => format!("FloatLit({f})"),
-            TokenKind::BoolLit(b) => format!("BoolLit({b})"),
-            other => format!("{other:?}"),
-        };
-        println!("line {:>3}:{:<3} | {label}", tok.line, tok.col);
-    }
+    let program = parser::parse(tokens);
+    println!("{program:#?}");
 }
